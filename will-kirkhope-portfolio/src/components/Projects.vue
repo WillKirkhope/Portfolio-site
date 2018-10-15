a944c7d97391fa16965cafee8d45cd04eeda1314
WillKirkhope <kirkhopetraining@yahoo.com>
2018-10-15 15:43:30 -0600

    added minor addition to projects section

--- will-kirkhope-portfolio/src/components/Projects.vue
+++ will-kirkhope-portfolio/src/components/Projects.vue
@@ -5,6 +5,12 @@
     </div>
     <h1>Projects</h1>
     <div class="project">
+      <a href="https://github.com/WillKirkhope/Capstone-FrontEnd">Fireteam Finder</a>
+      <p>Fireteam Finder is a full stack web application that serves as a social space for gamers to connect, link up and play their favorite games. Users can create posts for other players to see as well as chat with other players in the chat room. Users can also create a viewable profile the describes how they play as well as their favorite games, and include a brag video so other users can see a visual demonstration of skill.</p>
+      <p>Tech Used: HTML, CSS, JavaScript, Bootstrap, Firebase, VueJS, Heroku, PostgreSQL, Knex, Express, Git/Github, Socket.io</p>
+      <img src="../assets/FireteamFinder.png" alt="" class="ProjImg">
+    </div>
+    <div class="project">
       <a href="https://github.com/WillKirkhope/Borough-FrontEnd/tree/master/borough-fe">The Borough</a>
       <p>The Borough was an app created initially as a student project with a timeline of four days and the challenge of learning a new framework on top of that. The app is a meetup app geared towards getting community service events started and bringing knowledge about them to more people.</p>
       <p>Tech Used: HTML, CSS, JavaScript, Bootstrap, Firebase, VueJS, Heroku, PostgreSQL, Knex, Express, Git/Github, GoogleMaps</p>
